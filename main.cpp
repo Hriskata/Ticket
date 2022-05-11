6e63dd58ae68511e170158a729cd81eb61e2d3c5
Hriskata <105062473+Hriskata@users.noreply.github.com>
2022-05-11 04:38:09 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,32 @@
 #include "Hall.hpp"
-
+#include "Show.hpp"
 int main() {
-
-	Hall h1(1,2,3);
-	Hall h2(2,3,4);
-	Hall h3(h2);
-	h1.print();
+	//Hall h1(1,2,3);
+	//h1.print();
+	//try
+	//{
+	//	Hall h2(1, 2, 3);
+	//}
+	//catch (std::exception& e)
+	//{
+	//	std::cout << e.what()<<std::endl;
+	//}
+	//h2.print();
+	//Hall h3(22,3,4);
+	//h2.print();
+	//h3.print();
+	//Hall h3(h2);
+	//h1 = h2;
+	//h1.free_seats();
+	//h1.print();
+	//Show s1(3,"Show", "24-02-1999");
+	//s1.print();
+	//Show s2(s1);
+	//s2.print();
+	//s2.set_shows_name("Show2");
+	//s2.print();
+	//std::cout<<h1.get_number_of_hall();
+	//std::cout<<h1.get_arr_of_seats();
+	
 	return 0;
 }
