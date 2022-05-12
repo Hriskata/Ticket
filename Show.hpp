f8e75610cd56446382918abdcc63920b9a9279ea
Hriskata <105062473+Hriskata@users.noreply.github.com>
2022-05-13 02:45:24 +0300

    Update Show.hpp

--- Show.hpp
+++ Show.hpp
@@ -1,43 +1,31 @@
 #ifndef __SHOW__
 #define __SHOW__
 
+#include "Storage_Halls.hpp"
 #include "Hall.hpp"
-#include <cstring>
+#include <string>
+#include <vector>
 
-class Show : public Hall
+class Show : protected Storage_of_halls
 {
-	char* name;
-	char* date;
+	std::string number_of_hall;
+	std::string name;
+	std::string date;
 
-	bool CheckShow(const char* _name); // ++
-	// funkciq koqto shte mi proverqva dali datata e validna
-	bool CheckDate(const char* _date);
-	// prepravq datata na format dd-mm-yyyy ako ne e
-	void DateFormat(char* _date);
-	// dobavq data kum masiva s dati na edno predstavlenie
-	void AddDate(const char* _date); // ++
+	
 public:
-	// konstruktori, operator=, destruktor
-	Show();
-	Show(int _number_of_hall, const char* _name, const char* _date);
-	Show(const Show& other);
-	Show operator=(const Show& other);
+
+	Show() = default;
+	Show(std::string _date, std::string _name, std::string _number_of_hall,Storage_of_halls other);
 	~Show();
-	// geturi i seturi
-	const char* get_shows_name() const;
-	const char* get_shows_date() const;
-	int get_shows_number_of_hall() const;
+	//geturi
+	std::string get_Show_name();
+	std::string get_Show_date();
 
-	void set_shows_name(const char* _name);
-	void set_shows_date(const char* _date);
-	void set_shows_number_of_hall(int _number_of_hall);
 
-	// funkciq za dobavqne na predstavlenie, koito se suhranqvat vuv vektor
-	void AddShow();
-
-	// print funkciq
-	void print();
-
+	friend std::ostream& operator<<(std::ostream& os, const Show& show);
+	bool operator==(const Hall_of_the_Show& robj);
 };
 
-#endif
+#endif // !__SHOW__
+
