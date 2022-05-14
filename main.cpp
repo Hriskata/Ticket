05a5e867826dca6cd473bce4dfe8497dc2bb0b19
Hriskata <105062473+Hriskata@users.noreply.github.com>
2022-05-14 23:17:35 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,32 +1,21 @@
-#include "Hall.hpp"
-#include "Show.hpp"
-int main() {
-	//Hall h1(1,2,3);
-	//h1.print();
-	//try
-	//{
-	//	Hall h2(1, 2, 3);
-	//}
-	//catch (std::exception& e)
-	//{
-	//	std::cout << e.what()<<std::endl;
-	//}
-	//h2.print();
-	//Hall h3(22,3,4);
-	//h2.print();
-	//h3.print();
-	//Hall h3(h2);
-	//h1 = h2;
-	//h1.free_seats();
-	//h1.print();
-	//Show s1(3,"Show", "24-02-1999");
-	//s1.print();
-	//Show s2(s1);
-	//s2.print();
-	//s2.set_shows_name("Show2");
-	//s2.print();
-	//std::cout<<h1.get_number_of_hall();
-	//std::cout<<h1.get_arr_of_seats();
-	
+#include "Hall.h"
+#include "Show.h"
+#include "Storage.h"
+
+#include <iostream>
+
+int main()
+{
+	Storage storagee;
+	Hall zala1("huy", 1, 1);
+	storagee.Add_To_Storage_Of_Halls(zala1);
+	Hall h2("potka", 2, 2);
+	storagee.Add_To_Storage_Of_Halls(h2);
+	storagee.Size_On_Storage_Of_Halls();
+
+	zala1.PrintHall();
+	Show show1("24-02-1999", "aladin", zala1);
+	storagee.Add_New_Show(show1);
+
 	return 0;
 }
