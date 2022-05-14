b8ad6cbb19f595185589756f25a0daf919b0477b
Hriskata <105062473+Hriskata@users.noreply.github.com>
2022-05-14 23:12:45 +0300

    Update Hall.cpp

--- Hall.cpp
+++ Hall.cpp
@@ -1,83 +1,65 @@
-#include "Hall.hpp"
+#include "Hall.h"
 
 Hall::Hall()
 {
-	number_of_hall = '\0';
-	rows_of_hall = 0;
-	columns_of_hall = 0;
-	arr_of_seats = nullptr;
+	name_of_hall = '\0';
+	row_of_hall = 0;
+	seats_on_row = 0;
 }
 
-Hall::Hall(std::string _number_of_hall, int _rows, int _columns)
+Hall::Hall(std::string _name_of_hall, int _row_of_hall, int _seats_on_row)
 {
-	number_of_hall = _number_of_hall;
-	rows_of_hall = _rows;
-	columns_of_hall = _columns;
-
-	arr_of_seats = new int*[rows_of_hall];
-	for (size_t i = 0; i < rows_of_hall; i++) {
-		arr_of_seats[i] = new int[columns_of_hall];
-		for (size_t j = 0; j < columns_of_hall; j++)
-		{
-			arr_of_seats[i][j] = 0;
-		}
-	}
-	
+	name_of_hall = _name_of_hall;
+	row_of_hall = _row_of_hall;
+	seats_on_row = _seats_on_row;
 }
 
-//Hall::~Hall()
+std::string Hall::Get_name_of_hall() const
+{
+	return name_of_hall;
+}
+
+int Hall::Get_row_of_hall() const
+{
+	return row_of_hall;
+}
+
+int Hall::Get_seats_on_row() const
+{
+	return seats_on_row;
+}
+
+void Hall::Set_name_of_hall(std::string _name)
+{
+	name_of_hall = _name;
+}
+
+void Hall::Set_row_of_hall(int _row)
+{
+	row_of_hall = _row;
+}
+
+void Hall::Set_seats_on_row(int _seats)
+{
+	seats_on_row = _seats;
+}
+
+void Hall::PrintHall() const
+{
+	std::cout << "Name on hall : " << name_of_hall << std::endl;
+	std::cout << "Rows : " << row_of_hall << std::endl;
+	std::cout << "Seats on row : " << seats_on_row << std::endl;
+}
+
+//Hall& Hall::operator=(const Hall& other)
 //{
-//	for (size_t i = 0; i < rows_of_hall; ++i) 
+//	if (this != &other)
 //	{
-//		delete[] arr_of_seats[i];
+//		name_of_hall = other.name_of_hall;
+//		row_of_hall = other.row_of_hall;
+//		seats_on_row = other.seats_on_row;
 //	}
-//	delete[] arr_of_seats;
+//	return *this;
 //}
-
-std::string Hall::get_number_of_hall() const
-{
-	//std::cout<<number_of_hall<<std::endl;
-	return number_of_hall;
-}
-
-int Hall::get_arr_of_seats() const
-{
-	std::cout << "SEATS : "<<std::endl;
-	for (int i = 0; i < rows_of_hall; i++)
-	{
-		for (int j = 0; j < columns_of_hall; j++)
-		{
-			std::cout << arr_of_seats[i][j] << " | ";
-		}
-		std::cout << std::endl;
-	}
-	return 0;
-}
-
-int Hall::get_rows_of_hall() const
-{
-	return rows_of_hall;
-}
-
-int Hall::get_columns_of_hall() const
-{
-	return columns_of_hall;
-}
-
-void Hall::print_hall_info()
-{
-	for (size_t i = 0; i < Hall_info.size(); i++)
-	{
-		std::cout << Hall_info[i].data << std::endl;
-		std::cout << Hall_info[i].ShowsName << std::endl;
-	}
-}
-
-
-std::ostream& operator<<(std::ostream& os, const Hall& hall)
-{
-	os << hall.number_of_hall << hall.rows_of_hall << hall.columns_of_hall;
-	return os;
-}
 
 
