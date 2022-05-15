746a896eb30dd57184e5507035c35efe72899fb9
Hriskata <105062473+Hriskata@users.noreply.github.com>
2022-05-15 22:18:26 +0300

    Update Hall.h

--- Hall.h
+++ Hall.h
@@ -11,23 +11,28 @@ protected:
 	int seats_on_row;
 
 public:
-	// konstruktor po podrazbirane, konstruktor, destruktor
+	// конструктори
 	Hall();
 	Hall(std::string _name_of_hall, int _row_of_hall, int _seats_on_row);
 
-	// geturi
+	// Селектори
+	// Селектор за името на залата от клас зала
 	std::string Get_name_of_hall() const;
+	// Селектор за ред на залата от клас зала
 	int Get_row_of_hall() const;
+	// Селектор за местата на ред от клас зала
 	int Get_seats_on_row() const;
 
+	// Мутатори
+	// Мутатор за името на залата от клас зала
 	void Set_name_of_hall(std::string _name);
+	// Мутатор за реда на залата от клас зала
 	void Set_row_of_hall(int _row);
+	// Мутатор за местата на ред от клас зала
 	void Set_seats_on_row(int _seats);
-	// printira samata zala
-	void PrintHall() const;
 
-	// overload operator=
-	//Hall& operator=(const Hall& other);
+	// Принтира информация за залата от клас зала
+	void PrintHall() const;
 };
 
 #endif // !__HALL__
