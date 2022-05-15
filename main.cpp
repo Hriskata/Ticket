12bbe657832893683f4309bb1ec02ce609ccf0cf
Hriskata <105062473+Hriskata@users.noreply.github.com>
2022-05-15 22:21:14 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,21 +1,26 @@
-#include "Hall.h"
-#include "Show.h"
 #include "Storage.h"
-
 #include <iostream>
 
 int main()
 {
 	Storage storagee;
-	Hall zala1("huy", 1, 1);
+	Hall zala1("zala1", 1, 1);
 	storagee.Add_To_Storage_Of_Halls(zala1);
-	Hall h2("potka", 2, 2);
-	storagee.Add_To_Storage_Of_Halls(h2);
-	storagee.Size_On_Storage_Of_Halls();
+	Hall zala2("zala2", 2, 2);
+	storagee.Add_To_Storage_Of_Halls(zala2);
 
-	zala1.PrintHall();
-	Show show1("24-02-1999", "aladin", zala1);
-	storagee.Add_New_Show(show1);
+	try
+	{
+		storagee.RunTicket();
+	}
+	catch (std::exception& e)
+	{
+		std::cout << e.what() << '\n';
+	}
+
+	//zala1.PrintHall();
+	//Show show1("24-02-1999", "aladin", zala1);
+	//storagee.Add_New_Show(show1);
 
 	return 0;
 }
